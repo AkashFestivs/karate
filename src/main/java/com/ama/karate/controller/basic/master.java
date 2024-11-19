ae7da1ee61daf589111aab955a029a5e8a87f4b0
AkashFestivs <gadhaveakash999@gmail.com>
2024-11-20 00:49:54 +0530

    Commit

--- src/main/java/com/ama/karate/controller/basic/master.java
+++ src/main/java/com/ama/karate/controller/basic/master.java
@@ -6,16 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ama.karate.dto.BeltDto;
 import com.ama.karate.dto.ClassesDto;
+import com.ama.karate.dto.ResponseDto;
 import com.ama.karate.dto.StudentDto;
 import com.ama.karate.interfaceService.MasterInterfaceService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class Master {
@@ -26,7 +27,6 @@ public class Master {
 
     @PostMapping("/classes-list")
     public ResponseEntity<String> classesList() {
-
         try {
             List<ClassesDto> response = mis.bringClassList();
             String jsonResponse = om.writeValueAsString(response);
@@ -37,7 +37,7 @@ public class Master {
     }
 
     @PostMapping("/belt-list")
-    public ResponseEntity<String> beltList(HttpSession session) {
+    public ResponseEntity<String> beltList() {
 
         try {
             List<BeltDto> response = mis.bringBeltList();
@@ -49,7 +49,7 @@ public class Master {
     }
 
     @PostMapping("/all-students")
-    public ResponseEntity<String> allStudent(HttpSession session) {
+    public ResponseEntity<String> allStudent() {
 
         try {
             List<StudentDto> response = mis.bringAllStudents();
@@ -58,6 +58,30 @@ public class Master {
             return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
         } catch (JsonProcessingException e) {
             return new ResponseEntity<>("JsonProcessingException", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/add-classes")
+    public ResponseEntity<String> addClasses(@RequestBody String JsonObj) {
+
+        try {
+            ResponseDto response = mis.insertClasses(JsonObj);
+
+            return ResponseEntity.status(response.getStatusCode()).body(response.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/add-belts")
+    public ResponseEntity<String> addBelts(@RequestBody String JsonObj) {
+
+        try {
+            ResponseDto response = mis.insertBelt(JsonObj);
+
+            return ResponseEntity.status(response.getStatusCode()).body(response.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
         }
     }
 
